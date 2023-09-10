d1a2c744fb1fb1e8ab2f69541fe094ef41fa5a1e
briababby57 <96610248+briababby57@users.noreply.github.com>
2023-09-09 21:00:43 -0500

    Create Day 2-Control Flow.py

---
+++ 2.1-4 ControlFlow.md/Day 2/Day 2-Control Flow.py
@@ -0,0 +1,15 @@
+# Day 2 2.1
+
+
+## If, Elif, Else 
+
+#You can use if, elif (short for “else if”), and else to perform different actions based on certain conditions.__
+
+age = 
+if age < 12:
+    print("You are a minor.")
+elif age < 65:
+    print("You are an adult.")
+else:
+    print("You're a senior citizen.")
+````
